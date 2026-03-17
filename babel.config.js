1d74740689f66bb9dad12df24499083557782905
Abdullah Idrees <naziaidrees83@gmail.com>
2026-03-17 18:41:45 +0500

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   let plugins = [];
 
-  plugins.push("react-native-worklets/plugin");
+  // Safely add worklets plugin only if available
+  try {
+    require.resolve("react-native-worklets/plugin");
+    plugins.push("react-native-worklets/plugin");
+  } catch {
+    // Plugin not available, skip
+  }
+  // IMPORTANT: react-native-reanimated/plugin must be listed LAST
+  plugins.push("react-native-reanimated/plugin");
 
   return {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
